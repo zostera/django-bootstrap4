b32c14f9570f1b7bbb87fc9800ca4364e30b7387
Dylan Verheul <dylan@zostera.nl>
2017-05-24 08:31:59 +0200

    Fix demo INSTALLED_APPS to allow demo to work again

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -116,6 +116,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'django.contrib.admin',
 
     'bootstrap4',
     'demo',
