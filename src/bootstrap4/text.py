03621589760fed33ce04a5ee517eb9b34cd612cb
Jann Haber <jannh@selfnet.de>
2019-09-10 22:02:16 +0200

    Replace force_text with force_str - since Django 2.0, the two are aliases and force_text is deprecated with Django 3.0

--- src/bootstrap4/text.py
+++ src/bootstrap4/text.py
@@ -1,14 +1,11 @@
-try:
-    from django.utils.encoding import force_text
-except ImportError:
-    from django.utils.encoding import force_unicode as force_text
+from django.utils.encoding import force_str
 
 
 def text_value(value):
     """Force a value to text, render None as an empty string."""
     if value is None:
         return ""
-    return force_text(value)
+    return force_str(value)
 
 
 def text_concat(*args, **kwargs):
