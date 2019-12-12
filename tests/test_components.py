8c28038f6a028cf21a73267a0609bec396ea5303
Dylan Verheul <dylan@zostera.nl>
2019-12-12 12:01:21 +0100

    Update Makefile, reformat code, and add test

--- tests/test_components.py
+++ tests/test_components.py
@@ -2,47 +2,66 @@ from django.test import TestCase
 from django.utils.safestring import mark_safe
 
 from bootstrap4.components import render_alert
+from bootstrap4.exceptions import BootstrapError
+from bootstrap4.forms import render_button
 
 
-class ComponentsTest(TestCase):
+class AlertsTest(TestCase):
     def test_render_alert_without_type(self):
-        alert = render_alert("content")
         self.assertEqual(
-            alert,
-            '<div class="alert alert-info alert-dismissible" role="alert">'
-            + '<button type="button" class="close" data-dismiss="alert" '
-            + 'aria-label="close">'
-            + "&times;</button>content</div>",
+            render_alert("content"),
+            (
+                '<div class="alert alert-info alert-dismissible" role="alert">'
+                '<button type="button" class="close" data-dismiss="alert" aria-label="close">&times;</button>content'
+                "</div>"
+            ),
         )
 
     def test_render_alert_with_type(self):
-        alert = render_alert("content", alert_type="danger")
         self.assertEqual(
-            alert,
-            '<div class="alert alert-danger alert-dismissible" role="alert">'
-            + '<button type="button" class="close" data-dismiss="alert" '
-            + 'aria-label="close">'
-            + "&times;</button>content</div>",
+            render_alert("content", alert_type="danger"),
+            (
+                '<div class="alert alert-danger alert-dismissible" role="alert">'
+                '<button type="button" class="close" data-dismiss="alert" aria-label="close">&times;</button>'
+                "content"
+                "</div>"
+            ),
         )
 
     def test_render_alert_with_safe_content(self):
-        msg = mark_safe('This is <a href="https://example.com" class="alert-link">a safe link</a>!')
-        alert = render_alert(msg)
         self.assertEqual(
-            alert,
-            '<div class="alert alert-info alert-dismissible" role="alert">'
-            + '<button type="button" class="close" data-dismiss="alert" '
-            + 'aria-label="close">'
-            + '&times;</button>This is <a href="https://example.com" class="alert-link">a safe link</a>!</div>',
+            render_alert(mark_safe('This is <a href="https://example.com" class="alert-link">a safe link</a>!')),
+            (
+                '<div class="alert alert-info alert-dismissible" role="alert">'
+                '<button type="button" class="close" data-dismiss="alert" aria-label="close">&times;</button>'
+                'This is <a href="https://example.com" class="alert-link">a safe link</a>!'
+                "</div>"
+            ),
         )
 
     def test_render_alert_with_unsafe_content(self):
-        msg = "This is <b>unsafe</b>!"
-        alert = render_alert(msg)
         self.assertEqual(
-            alert,
-            '<div class="alert alert-info alert-dismissible" role="alert">'
-            + '<button type="button" class="close" data-dismiss="alert" '
-            + 'aria-label="close">'
-            + "&times;</button>This is &lt;b&gt;unsafe&lt;/b&gt;!</div>",
+            render_alert("This is <b>unsafe</b>!"),
+            (
+                '<div class="alert alert-info alert-dismissible" role="alert">'
+                '<button type="button" class="close" data-dismiss="alert" aria-label="close">&times;</button>'
+                "This is &lt;b&gt;unsafe&lt;/b&gt;!"
+                "</div>"
+            ),
         )
+
+
+class ButtonsTest(TestCase):
+    def test_button(self):
+        self.assertEqual(render_button("button"), '<button class="btn btn-primary">button</button>')
+
+    def test_button_with_illegal_type(self):
+        try:
+            self.assertEqual(
+                render_button("button", button_type="illegal"), '<button class="btn btn-primary">button</button>'
+            )
+        except BootstrapError as e:
+            self.assertEqual(
+                str(e),
+                'Parameter "button_type" should be "submit", "reset", "button", "link" or empty ("illegal" given).',
+            )
