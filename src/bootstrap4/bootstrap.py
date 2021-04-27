4cbe055fc2c1f58b667e29fb0e4d2c6bfba9ceaf
Vlad Vasiliu <vladvasiliun@gmail.com>
2021-04-27 11:17:42 +0200

    Include JS bundle instead of separate script (#279)

--- src/bootstrap4/bootstrap.py
+++ src/bootstrap4/bootstrap.py
@@ -9,8 +9,8 @@ BOOTSTRAP4_DEFAULTS = {
         "crossorigin": "anonymous",
     },
     "javascript_url": {
-        "url": "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js",
-        "integrity": "sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF",
+        "url": "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js",
+        "integrity": "sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns",
         "crossorigin": "anonymous",
     },
     "theme_url": None,
