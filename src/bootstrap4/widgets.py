8638a5da6e631ea151a0b71f36e44ec1fa5ec818
Raylan <jeffersonraylan@gmail.com>
2021-10-23 13:07:06 -0300

    Added Widgets to make use of HTML5's date/time picker
    Widgets:
     * DateTimeWidget
     * DateWidget
     * TimeWidget

--- src/bootstrap4/widgets.py
+++ src/bootstrap4/widgets.py
@@ -1,5 +1,9 @@
-from django.forms import RadioSelect
-
+from django.forms import(
+    RadioSelect,
+    DateInput,
+    DateTimeInput,
+    TimeInput
+)
 
 class RadioSelectButtonGroup(RadioSelect):
     """
@@ -9,3 +13,31 @@ class RadioSelectButtonGroup(RadioSelect):
     """
 
     template_name = "bootstrap4/widgets/radio_select_button_group.html"
+
+
+class DateWidget(DateInput):
+    """
+    This widget renders a HML5 date field type and enables date picker.
+    """
+
+    input_type = "date"
+
+    def format_value(self, value):
+        return str(value or '')
+
+class DateTimeWidget(DateTimeInput):
+    """
+    This widget renders a HML5 datetime-local field type and enables date and time picker.
+    """
+
+    input_type = "datetime-local"
+    template_name = "bootstrap4/widgets/datetime_field.html"
+    def format_value(self, value):
+        return 'T'.join(str(value or '').split())
+
+class TimeWidget(TimeInput):
+    """
+    This widget renders a HML5 time field type and enables time picker.
+    """
+
+    input_type = "time"
