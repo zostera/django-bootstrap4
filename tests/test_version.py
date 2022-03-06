df541121c3081907dacb41872c5163bf28ed3de6
Dylan Verheul <dylan@dyve.net>
2022-03-06 10:27:46 +0100

    Fixed version test

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,8 +5,8 @@ class VersionTest(TestCase):
     """Test presence of package version."""
 
     def test_version(self):
-        import bootstrap4
+        import django_bootstrap4
 
-        version = bootstrap4.__version__
+        version = django_bootstrap4.__version__
         version_parts = version.split(".")
         self.assertEqual(len(version_parts), 2)
