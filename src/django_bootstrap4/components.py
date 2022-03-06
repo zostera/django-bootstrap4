1b69d91e690e5d3ab7915a8f3b45ea9ed0cf4a17
Dylan Verheul <dylan@dyve.net>
2022-03-06 10:27:03 +0100

    Fixed alerts

--- src/django_bootstrap4/components.py
+++ src/django_bootstrap4/components.py
@@ -1,6 +1,4 @@
-from django.template.defaultfilters import capfirst
 from django.utils.html import format_html
-from django.utils.translation import gettext as _
 
 from .css import merge_css_classes
 from .html import render_tag
@@ -16,14 +14,13 @@ def render_alert(content, alert_type="info", dismissible=True, extra_classes="")
         raise ValueError(f"Value {alert_type} is not a valid alert type. Please choose from {', '.join(ALERT_TYPES)}.")
     css_classes = [f"alert alert-{alert_type}"]
     if dismissible:
-        css_classes.append("alert-dismissible fade show")
-        close = capfirst(_("close"))
-        button = f'<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="{close}"></button>'
+        css_classes.append("alert-dismissible")
+        button = f'<button type="button" class="close" data-dismiss="alert" aria-label="close">&times;</button>'
     css_classes = merge_css_classes(*css_classes, extra_classes)
     return render_tag(
         "div",
         attrs={"class": css_classes, "role": "alert"},
-        content=format_html("{content}" + button, content=content),
+        content=format_html(button + "{content}", content=content),
     )
 
 
