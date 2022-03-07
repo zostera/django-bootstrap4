cd3fea78042a31f9786bcf11f5b6f5403230c7f0
Dylan Verheul <dylan@dyve.net>
2022-03-07 08:34:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         "Framework :: Django",
         "Framework :: Django :: 2.2",
         "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
